7ce36c030eb3d039104bbc2cd086e04579b333ec
dannycod3r <oseid7807@gmail.com>
2023-02-01 06:34:23 -0800

    update init.el with basic emacs config

--- init.el
+++ init.el
@@ -1,3 +1,17 @@
-;; create a file ~/.emacs.d/init.el
+;; create a file ~/.config/emacs/init.el
 ;; populate with the content of this file
+
+;; personal config
+
+;; set theme
+(load-theme 'tango-dark)
+
+(set-fringe-mode 10) ;; give space
+
+;; autoclose brackets
+(require 'cc-mode)
+(add-hook 'c-mode-common-hook 'electric-pair-mode)
+
+;; line number in all buffers
+(global-display-line-numbers-mode 1)
 
